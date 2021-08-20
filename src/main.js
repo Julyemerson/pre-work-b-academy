241314bf815a7bd0c440f2c5dad95fffc9b86c93
Julyemerson Leonizio <jb.leonizio@gmail.com>
2021-08-20 17:26:33 -0300

    capitalize finalizado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+import "./form";
 
+const inputText = document.querySelector('[data-js="inputName"]');
+const prohibetedWord = ["de", "da", "do", "dos"];
+
+function capitalize(str) {
+  const auxArray = str.currentTarget.value.split(" ");
+  const capitalized = auxArray
+    .map((item) => {
+      return item.charAt(0).toUpperCase() + item.slice(1);
+    })
+    .join()
+    .replaceAll(",", " ");
+
+  str.currentTarget.value = capitalized;
+}
+
+inputText.addEventListener("input", capitalize);
